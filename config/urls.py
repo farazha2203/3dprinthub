3d12a7430b03aa48cf074e59195033e7e6bdda74
farazha2203 <102502265+farazha2203@users.noreply.github.com>
2026-08-25 17:04:59 +0330

    feat(50a): route finance command center through admin auth

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.urls import include, path, re_path
 from store.public_media import serve_public_store_media
 from store.epic49_routes import product_detail_by_id_view, product_detail_compat_view
 from store.sitemaps import CategorySitemap, ProductSitemap, ServicePageSitemap, StaticViewSitemap
+from website.phase50a_admin_command_center import phase50_admin_command_center
 from django_smartbase_admin.admin.site import sb_admin_site
 admin.site.site_header = "مدیریت 3DPrintHub"
 admin.site.site_title = "3DPrintHub"
@@ -31,6 +32,11 @@ urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("accounts/", include("allauth.urls")),
+    path(
+        "admin/command-center/",
+        admin.site.admin_view(phase50_admin_command_center),
+        name="phase50_admin_command_center",
+    ),
     path("admin/", admin.site.urls),
     path("smart-admin/", sb_admin_site.urls),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
